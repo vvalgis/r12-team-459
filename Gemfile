a4665cd7b6688de78ee72830fe1101df111731ce
Vladimir Valgis <vladimir.valgis@gmail.com>
2012-10-13 04:57:15 +0400

    Add rails

--- Gemfile
+++ Gemfile
@@ -1,4 +1,14 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-# gem "rails"
+gem "rails"
+gem "mysql2"
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'haml-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R12Team459::Application.config.secret_token = '3ee45089cb5d5a083dc7cf469891f3505ae7010ef04ada0d7d724799b5473c8dd0118f3f02b4498f0f745dabd7749e9a2dfaeb68e34153eb27294ff3a69e4fae'
