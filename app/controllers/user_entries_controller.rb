e238e5e3118ba30b9604100b657e6687f17f6728
Rinat Shaykhutdinov <rinat.crone@gmail.com>
2012-10-15 01:57:03 +0400

    Controller improvements

--- app/controllers/user_entries_controller.rb
+++ app/controllers/user_entries_controller.rb
@@ -7,11 +7,8 @@ class UserEntriesController < InheritedResources::Base
     params[:user_entry][:user_id] = current_user.id
     entry = params[:user_entry]
     if entry[:categories].present?
-      categories = entry[:categories].split(',').map do|name|
+      categories = entry[:categories].split(',').map(&:strip).map do |name|
         current_user.categories.find_or_create_by_name(name).id
-      end
-      categories.each do |cat_id|
-
       end
     end
   end
